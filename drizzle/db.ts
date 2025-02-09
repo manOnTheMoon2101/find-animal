824519120e4172e72dbba30d98b3d26561ec12b0
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-02-09 14:41:07 +0200

    updated type shit

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,8 +1,15 @@
+// src/db.ts
+
 import { drizzle } from "drizzle-orm/neon-http";
+
 import { neon } from "@neondatabase/serverless";
+
 import { config } from "dotenv";
 
-config({ path: ".env" });
+
+config({ path: ".env" }); // or .env.local
+
 
 const sql = neon(process.env.DATABASE_URL!);
+
 export const db = drizzle({ client: sql });
